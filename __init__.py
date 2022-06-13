f7ad26f8cbb8f2fbd048244c5b246a8fcd38b97c
Kareem <kareemmahlees@gmail.com>
2022-06-13 21:24:20 +0200

    base init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+FOR A LESS BOARING LIFE 
+
+--------------------------------------------------
+
+Quote me is a telegram bot that sends you daily quotes to add little bit of fun to your day 
+
+APIs used :
+    >>> quote.rest
+    >>> unsplash api 
+
+"""
